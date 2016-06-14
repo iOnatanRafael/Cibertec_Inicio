27927b655751c5777e7a9f06207adb6c01f5f660
Jhonatan Rafael Goñe Bicharra <jhonatan.gone@hotmail.com>
2016-06-13 22:29:20 -0500

    13062016 Creación de DbContext, configuración de connection string, proyectos base para Modelos y DataAcces

---
+++ WebDeveloper.DataAcces/ClientData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAcces
+{
+    public class ClientData
+    {
+        public List<Client> GetFakeData()
+        {
+            return new List<Client>
+            {
+                new Client { ID=1, Name="Juan", LastName="Perez" },
+                new Client { ID=2, Name="Raul", LastName="Cuidiaz" }
+            };
+        }
+
+        public List<Client> GetList()
+        {
+            using (var dbContext = new WebContextDb())
+            {
+                return dbContext.Clients.ToList();
+            }
+        }
+
+    }
+}
---
+++ WebDeveloper.DataAcces/WebContextDb.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAcces
+{
+    public class WebContextDb : DbContext
+    {
+        public WebContextDb() : base("name=WebDeveloperConnectionString")
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
