b3b71e49f0268fac2d9920951e01e59edc466dd8
Jhonatan Rafael Goñe Bicharra <jhonatan.gone@hotmail.com>
2016-06-18 23:15:09 -0500

    Sincronización desde otra PC a GitHub

---
+++ WebDeveloper.DataAcces/ClientData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAcces
+{
+    public class ClientData : BaseDataAccess<Client>
+    {
+        public Client GetClientById(int id)
+        {
+            using (var dbContext = new WebContextDb())
+            {
+                return dbContext.Clients.FirstOrDefault(x => x.ID == id);
+            }
+        }
+    }
+}
---
+++ WebDeveloper/Controllers/ClientController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.DataAcces;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Controllers
+{
+    public class ClientController : Controller
+    {
+        private ClientData _client = new ClientData();
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View(_client.GetList());
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            if(ModelState.IsValid)
+            {
+                _client.Add(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int ID)
+        {
+            Client client = _client.GetClientById(ID);
+            if (client == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _client.Update(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            Client client = _client.GetClientById(ID);
+            if (client == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Client client)
+        {       
+            if(_client.Delete(client)>0)
+            return RedirectToAction("Index");
+            return View(client);
+        }
+
+    }
+}
