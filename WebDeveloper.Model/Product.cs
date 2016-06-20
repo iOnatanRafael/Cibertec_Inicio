8368c23e7f6dfbac53c792a4755dee87cc47ae71
Jhonatan Rafael Goñe Bicharra <jhonatan.gone@hotmail.com>
2016-06-20 16:33:15 -0500

    Tarea: Se agregó el Modelo Producto con 5 propiedades. Se le creó su Controlador y sus respectivas Vistas.

--- WebDeveloper.Model/Product.cs
+++ WebDeveloper.Model/Product.cs
@@ -6,18 +6,19 @@ namespace WebDeveloper.Model
     public class Product
     {
         public int ID { get; set; }
+
         [Display(Name = "Name")]
         [Required(ErrorMessage = "This field is required")]
-
         public string Name { get; set; }
+
         [Display(Name = "Stock")]
         [Required(ErrorMessage = "This field is required")]
-
         public decimal Stock { get; set; }
+
         [Display(Name = "Price")]
         [Required(ErrorMessage = "This field is required")]
-
         public decimal Price { get; set; }
+
         public DateTime? DateRegistration { get; set; }
 }
 }
