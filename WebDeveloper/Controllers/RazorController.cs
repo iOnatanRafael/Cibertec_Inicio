3519d4ccff83e1ff6dd79771732d00f648f45016
Jhonatan Rafael Goñe Bicharra <jhonatan.gone@hotmail.com>
2016-06-20 22:29:33 -0500

    Uso de Razor

---
+++ WebDeveloper/Controllers/RazorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Controllers
+{
+    public class RazorController : Controller
+    {
+        // GET: Razor
+        public ActionResult Index()            
+        {
+            var client = new Client()
+            {
+                ID = 1,
+                LastName = "Cueva",
+                Name = "Aladino"
+            };
+            return View(client);
+        }
+    }
+}
